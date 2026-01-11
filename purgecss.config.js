24a4d55971e8f57abd997c96d8f85d402813c82d
Zhihao <7567311+liuh886@users.noreply.github.com>
2026-01-11 20:24:20 +0800

    remove gamefile form gitignore

--- purgecss.config.js
+++ purgecss.config.js
@@ -6,6 +6,23 @@ module.exports = {
     css: [
         "_site/assets/css/*.css"
     ],
+    safelist: [
+        "active",
+        "collapsed",
+        "dark",
+        "light",
+        "show",
+        /^navbar-/,
+        /^btn-/,
+        /^dropdown-/,
+        /^modal-/,
+        /^tooltip-/,
+        /^popover-/,
+        /^collapse/,
+        /^toast-/,
+        /^tab-/,
+        /^carousel-/
+    ],
     output: "_site/assets/css/",
     skippedContentGlobs: [
         "_site/assets/**/*.html"
