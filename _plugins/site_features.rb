5e0dd3dac46a3f36d521f6050fe991e67e9f6e16
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2026-08-13 14:18:46 +0800

    fix: gate search flags on search plugin

--- _plugins/site_features.rb
+++ _plugins/site_features.rb
@@ -4,11 +4,13 @@ module SiteFeatures
   def self.apply(site)
     features = site.data.fetch("site_features", {})
     search = features.fetch("search", {})
+    search_available = Array(site.config["plugins"]).include?("al_search")
+    search_enabled = search.fetch("enabled", false) && search_available
 
-    site.config["search_enabled"] = search.fetch("enabled", false)
-    site.config["posts_in_search"] = search.fetch("posts", false)
-    site.config["socials_in_search"] = search.fetch("socials", false)
-    site.config["bib_search"] = search.fetch("bibliography", false)
+    site.config["search_enabled"] = search_enabled
+    site.config["posts_in_search"] = search_enabled && search.fetch("posts", false)
+    site.config["socials_in_search"] = search_enabled && search.fetch("socials", false)
+    site.config["bib_search"] = search_enabled && search.fetch("bibliography", false)
   end
 end
 
